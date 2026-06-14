4e394abab60707327212ea4a24dd6dc34bbd125a
Wassim Assidi <assidi.wassim.fsm@hotmail.com>
2026-06-14 21:06:14 +0100

    change endpoint hello

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("Hello Le bon developpeur Community ! - new feature 🚀");
+  res.send("Hello Team");
 });
 
 app.get("/health", async (req, res) => {
